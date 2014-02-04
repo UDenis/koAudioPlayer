023ed2172dda854e807a05c3d4b83a2b28be2d69
Denis Maslov <denphobos@gmail.com>
2014-02-04 17:17:42 +0600

    refactoring

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,200 +1,146 @@
 (function(ko) {
 
   var playerTemplate = "" +
-    "<input class='range' type='range' min=0 step=1 data-bind='value:currentTime, attr:{max:duration}, event:{change:timeChanging}'></input>" +
+    "<input class='range' type='range' min=0 step=1 data-bind='value:slider().currentValue, attr:{max:slider().total}, event:{change:slider().timeChanging}'></input>" +
     "<button type='button' data-bind='click:play, visible:isPause()' class='btn play'></button>" +
     "<button type='button' data-bind='click:pause, visible:!isPause()' class='btn pause'></button>" +
     "<button type='button' data-bind='click:mute, css:{active:isMute}' class='btn mute'></button>" +
     "<button type='button' data-bind='click:prev' class='btn prev'></button>" +
     "<button type='button' data-bind='click:next' class='btn next'></button>" +
-    "<span  class='trackName' data-bind='text:trackName'></span>";
+    "<span  class='trackName' data-bind='text:track() && track().name'></span>";
 
   var isFunction = function(f) {
     return typeof f == "function";
-  }
+  };
 
-  var audioPlayers = [];
-  var ViewModel = function() {
+  var Track = function(url, name) {
+    this.url = url;
+    this.name = name;
+  };
+
+  var Slider = function() {
     var self = this;
-    var audio = this.audio = new Audio();
-    audioPlayers.push(this.audio);
-    this.__audioPlayerId = audioPlayers.length - 1;
 
-    // длительность текущего трека
-    this.duration = ko.observable(0);
+    // current track duration
+    this.total = ko.observable(0);
 
-    // текущее время проигрываемого трека
-    this.currentTime = ko.observable(0);
+    // current track timel
+    this.currentValue = ko.observable(0);
+
+    // handled when range input draging
+    this.timeChanging = function() {
+      var player = this;
+
+      self.manualChanging = true;
+      if (self._timer)
+        clearTimeout(self._timer);
+
+      self._timer = setTimeout(function() {
+        player.timeChanged(self.currentValue());
+        self.manualChanging = false;
+      }, 500);
+    };
+  };
+
+  Slider.prototype.setCurrentValue = function(value) {
+    !this.manualChanging && (this.currentValue(value));
+  };
+
+  var Player = function() {
+
+    this.audio = new Audio();
+
+    this.slider = ko.observable(new Slider());
+
+    this.track = ko.observable();
+    this.track.subscribe(function(track) {
+      this.audio.src = track.url;
+    }, this);
 
     this.isMute = ko.observable(false);
     this.isMute.subscribe(function(value) {
       if (value)
-        audio.volume = 0;
+        this.audio.volume = 0;
       else
-        audio.volume = 1
-    });
-
-    this.isPause = ko.observable(true);
-
-    // индекс проигрываемого файла в плей листе
-    this.currentAudioFileListIndex = ko.observable();
-
-    // проигрываемый файл       
-    this.audioFile = ko.observable();
-    this.audioFile.subscribe(function(value) {
-      audio.src = value.url;
-    });
-
-    this.currentAudioFileListIndex.subscribe(function(value) {
-      if (self.audioList)
-        self.audioFile(self.audioList[value]);
-    });
-
-    this.trackName = ko.computed(function() {
-      return (this.audioFile() || {}).name || "";
+        this.audio.volume = 1
     }, this);
 
+    this.isPause = ko.observable(true);
     this.initEvents();
-
   };
 
-  ViewModel.prototype.timeChanged = function() {
-    this.audio.currentTime = this.currentTime();
-  };
-
-  ViewModel.prototype.timeChanging = function() {
-    var self = this;
-    console.log("timeChanging");
-    this.manualChanging = true;
-    if (this.timer)
-      clearTimeout(this.timer);
-
-    this.timer = setTimeout(function() {
-      self.timeChanged();
-      console.log("timeChanged");
-      self.manualChanging = false;
-    }, 500);
-  }
-
-  ViewModel.prototype.play = function() {
-    for (var i = 0; i < audioPlayers.length; i++) {
-      audioPlayers[i].pause();
-    }
+  Player.prototype.play = function() {
     this.audio.play()
   };
 
-  ViewModel.prototype.pause = function() {
+  Player.prototype.pause = function() {
     this.audio.pause();
   };
 
-
-  ViewModel.prototype.mute = function() {
+  Player.prototype.mute = function() {
     this.isMute(!this.isMute());
   };
 
-  ViewModel.prototype.nextTrackInList = function() {
-    var audio = this.audio;
-    var autoPlay = audio.autoplay;
-    audio.autoplay = true;
-    audio.currentTime = 0;
-    if (this.currentAudioFileListIndex() == this.audioList.length - 1)
-      this.currentAudioFileListIndex(0)
-    else {
-      this.currentAudioFileListIndex(this.currentAudioFileListIndex() + 1);
-    }
-    audio.play();
-    audio.autoplay = autoPlay;
+  Player.prototype.timeChanged = function(time) {
+    this.audio.currentTime = time;
   };
 
-  ViewModel.prototype.prevTrackInList = function() {
-    var audio = this.audio;
-    var autoPlay = audio.autoplay;
-    audio.autoplay = true;
-    audio.currentTime = 0;
-    if (this.currentAudioFileListIndex() == 0)
-      this.currentAudioFileListIndex(this.audioList.length - 1);
-    else {
-      this.currentAudioFileListIndex(this.currentAudioFileListIndex() - 1);
+  Player.prototype.callbackProxy = function(fn, viewModel) {
+    var self = this;
+    return function() {
+      isFunction(fn) && fn.apply(viewModel);
     }
-    audio.play();
-    audio.autoplay = autoPlay;
   };
 
+  Player.prototype.createEventsProxy = function(events, viewModel) {
+    var self = this;
+    return function(eventName, data) {
+      var fn = events[eventName];
+      isFunction(fn) && fn.call(viewModel, data);
+    };
+  };
 
-  ViewModel.prototype.initEvents = function() {
+  Player.prototype.initEvents = function() {
     var self = this;
     var audio = this.audio;
 
-    // флаг, ползунок перетаскивается мышкой
-    this.manualChanging = false;
     audio.addEventListener('timeupdate', function() {
-      self.duration(audio.duration);
-      if (!self.manualChanging)
-        self.currentTime(audio.currentTime);
+      self.slider().setCurrentValue(audio.currentTime);
+      self.slider().total(audio.duration);
+      self.eventProxy("timeupdate")
     });
 
     audio.addEventListener('play', function() {
       self.isPause(false);
+      self.eventProxy("play")
     });
 
     audio.addEventListener('pause', function() {
       self.isPause(true);
+      self.eventProxy("pause")
     });
 
     audio.addEventListener('ended', function() {
-      if (self.audioListNotEmpty) {
-        self.nextTrackInList();
-      } else {
-        audio.currentTime = 0;
-        self.isPause(true);
-      }
+      audio.currentTime = 0;
+      self.eventProxy("ended")
     });
   };
 
-  ViewModel.prototype.initOrUpdate = function(valueAccessor, allBindings, viewModel) {
-    var audio = this.audio;
+  Player.prototype.initOrUpdate = function(valueAccessor, allBindings, viewModel) {
 
-    // объект аудио файла 
-    // { url : URL, name :trackName }
-    var audioFile = ko.utils.unwrapObservable(allBindings().audioFile);
+    this.track(ko.utils.unwrapObservable(allBindings().audioFile));
 
-    // массив объектов audioFile (плейлист)
-    var audioList = this.audioList = allBindings().audioList ? ko.utils.unwrapObservable(allBindings().audioList) : null
-    this.audioListNotEmpty = audioList && audioList.length > 0;
+    this.audio.autoplay = ko.utils.unwrapObservable(allBindings().autoplay);
 
-    if (audioFile) {
-      this.audioFile(audioFile);
-    } else if (this.audioListNotEmpty) {
-      this.currentAudioFileListIndex(0);
+    if (this.audio.autoplay) {
+      this.play();
     }
 
     // обработчики на нажатие следующий/предыдущий трек
-    this.toNext = ko.utils.unwrapObservable(allBindings().nextAudio);
-    this.toPrev = ko.utils.unwrapObservable(allBindings().prevAudio);
-
-    if (audio.autoplay) {
-      audio.play();
-    }
-
-    this.next = function() {
-      if (this.audioListNotEmpty)
-        this.nextTrackInList();
-      var autoPlay = this.audio.autoplay;
-      this.audio.autoplay = true;
-      isFunction(this.toNext) && this.toNext.apply(viewModel);
-      this.audio.autoplay = autoPlay;
-    };
-
-    this.prev = function() {
-      if (this.audioListNotEmpty)
-        this.prevTrackInList();
-      var autoPlay = this.audio.autoplay;
-      this.audio.autoplay = true;
-      isFunction(this.toPrev) && this.toPrev.apply(viewModel);
-      this.audio.autoplay = autoPlay;
-    };
+    this.eventProxy = this.createEventsProxy(ko.utils.unwrapObservable(allBindings().events), viewModel);
+    this.next = this.callbackProxy(ko.utils.unwrapObservable(allBindings().nextAudio), viewModel);
+    this.prev = this.callbackProxy(ko.utils.unwrapObservable(allBindings().prevAudio), viewModel);
   };
-
 
   var renderPlayer = function(element, valueAccessor, allBindings, viewModel, bindingContext) {
     element.innerHTML = playerTemplate;
@@ -210,7 +156,7 @@
 
   var getAccessor = function(valueAccessor, allBindings, viewModel, updating) {
     return function() {
-      var vm = allBindings.audioPlayerVM = allBindings.audioPlayerVM || new ViewModel();
+      var vm = allBindings.audioPlayerVM = allBindings.audioPlayerVM || new Player();
       vm.initOrUpdate(valueAccessor, allBindings, viewModel);
       return {
         templateEngine: ko.nativeTemplateEngine.instance,
@@ -218,7 +164,6 @@
       };
     };
   };
-
 
   ko.bindingHandlers.audioPlayer = {
     init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
--- audioPlayerExample.js
+++ audioPlayerExample.js
@@ -1,41 +1,40 @@
 	var srcList = [{
-		url:"1.mp3",
-		name : "Лучь солнца золотого"
-	},
-	{
-		url : "2.mp3",
-		name : "Сказка"
-	}
-	, 
-	{
-		url:"3.mp3",
-		name : "Песня мамонтенка"
-	}
-	];
+	  url: "http://elisto04f06.music.yandex.ru/get-mp3/247954a7b5e5fd7c7c17d0ac495355ca/4f18fe10a0a54/14/data-0.16:15147579583:4817396?track-id=2488075&from=service-30-track-album&similarities-experiment=empty",
+	  name: "Лучь солнца золотого"
+	}, {
+	  url: "http://elisto06e.music.yandex.ru/get-mp3/d2ee728bb17646ee97f94d3a7884790c/4f18fe9bd7f79/44/data-0.15:34909974105:5046229?track-id=2488082&from=service-10-track-album&similarities-experiment=empty",
+	  name: "Сказка"
+	}, {
+	  url: "http://elisto03f06.music.yandex.ru/get-mp3/f328b2befa502d4b7640bff50a676363/4f18fe822ddba/13/data-0.2:46054367832:4145945?track-id=2488076&from=service-10-track-album&similarities-experiment=empty",
+	  name: "Песня мамонтенка"
+	}];
 
-	var viewModel = function(){
-		this.currentIndex =ko.observable(0),
-		this.audioList = srcList;
-		this.curentFile = ko.computed(function(){
-			return srcList[this.currentIndex()];
-		}, this),
+	var viewModel = function() {
+	  this.currentIndex = ko.observable(0),
+	  this.audioList = srcList;
+	  this.curentFile = ko.computed(function() {
+	    return srcList[this.currentIndex()];
+	  }, this);
 
-		this.next = function()
-		{
-			if (this.currentIndex() == srcList.length-1)
-				this.currentIndex(0);
-			else
-				this.currentIndex(this.currentIndex()+1);
-		},
+	  this.autoplay = ko.observable(true);
 
-		this.prev =function()
-		{
-			if (this.currentIndex() == 0)
-				this.currentIndex(srcList.length-1);
-			else
-				this.currentIndex(this.currentIndex()-1);
-		}
+	  this.next = function() {
+	    if (this.currentIndex() == srcList.length - 1)
+	      this.currentIndex(0);
+	    else
+	      this.currentIndex(this.currentIndex() + 1);
+	  },
 
+	  this.prev = function() {
+	    if (this.currentIndex() == 0)
+	      this.currentIndex(srcList.length - 1);
+	    else
+	      this.currentIndex(this.currentIndex() - 1);
+	  },
+
+	  this.ended = function() {
+	    this.next();
+	  }
 	}
 
 	ko.applyBindings(new viewModel());
